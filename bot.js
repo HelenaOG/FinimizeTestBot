c7c5c39d64312ce54e87915646ff00ef69915604
HelenaOG <94908384+HelenaOG@users.noreply.github.com>
2021-11-26 09:44:13 +0000

    Initial commit

---
+++ bot.js
@@ -0,0 +1,22 @@
+const { Client, Intents } = require('discord.js');
+const keepAlive = require("./server")
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const token = process.env['token']
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ziz') {
+    await interaction.reply('yo!');
+  } else if (interaction.commandName === 'video') {
+    await interaction.reply('https://www.youtube.com/playlist?list=UUIuFRHmDktSjlHex7Hahcug&playnext=1&index=1');
+  }
+});
+
+keepAlive()
+client.login(token);
